c4a4cd98d89575bf9be09ccae4ba6bb479f46314
Rohit Ramname <rohit_ramname@hotmail.com>
2018-08-23 23:28:43 -0400

    changes

--- server.js
+++ server.js
@@ -9,11 +9,7 @@ child = exec(__dirname+'//node_modules//.bin//ng  build', function(error,stdout,
       console.log(error.stack); 
     
       }
-
-  });
-} 
-build();
-const express = require('express');
+      const express = require('express');
         
 const port=process.env.PORT || 3000
 
@@ -22,3 +18,8 @@ const app=express();
 
 app.use(express.static("dist"))
 app.listen(port,function(){console.log('server started')});
+
+  });
+} 
+build();
+
